86b8e5c9d77e0ea7ef2093ebc962003687ea5ed0
Li, Liang A <liang.a.li@intel.com>
2024-06-16 04:09:21 +0000

    add -l to specify local gpu index, -r to specify remote gpu index, -n to specify data length

--- lz_p2p/p2p.cpp
+++ lz_p2p/p2p.cpp
@@ -28,7 +28,7 @@ int parseInput(const std::string& input) {
 
     for (int i = 0; i < length; ++i) {
         if (!std::isdigit(input[i])) {
-            std::cerr << "ERROR: Invalid input (requires number or number with k or m, e.g., 256, 2k, 4m)" << std::endl;
+            std::cerr << "ERROR: Invalid input (-n requires a number or number with k or m, e.g., 256, 2k, 4m)" << std::endl;
             exit(-1);
         }
     }
@@ -38,29 +38,74 @@ int parseInput(const std::string& input) {
     return number * multiplier;
 }
 
+void parseCommandLine(int argc, char* argv[], int& local, int& remote, int& n) {
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg == "-l") {
+            if (i + 1 < argc) { // check if next parameter exists
+                local = std::atoi(argv[++i]);
+                if (local != 0 && local != 1) {
+                    std::cerr << "ERROR: -l must be 0 or 1." << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else {
+                std::cerr << "ERROR: -l requires a number." << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        else if (arg == "-r") {
+            if (i + 1 < argc) { // check if next parameter exists
+                remote = std::atoi(argv[++i]);
+                if (remote != 0 && remote != 1) {
+                    std::cerr << "ERROR: -r must be 0 or 1." << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else {
+                std::cerr << "ERROR: -r requires a number." << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        else if (arg == "-n") {
+            if (i + 1 < argc) { // check if next parameter exists
+                n = parseInput(argv[++i]);
+            }
+            else {
+                std::cerr << "ERROR: -n requires a number." << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        else {
+            std::cerr << "ERROR: Invalid argument." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
 int main(int argc, char** argv)
 {
-    int elem_num = 1024;
-    if (argc >= 2)
-        elem_num = parseInput(argv[1]);
-    printf("Number of test data = %d, size = %d\n", elem_num, elem_num * sizeof(uint32_t));
+    int local_gpu = 0, remote_gpu = 1, data_count = 1024;
+    parseCommandLine(argc, argv, local_gpu, remote_gpu, data_count);
+    printf("#### Input parameters: loca_ gpu idx = %d, remote_gpu idx = %d, data_count = %d\n", local_gpu, remote_gpu, data_count);
 
     lzContext ctx0, ctx1;
-    ctx0.initZe(0);
-    ctx1.initZe(1);
+    ctx0.initZe(local_gpu);
+    ctx1.initZe(remote_gpu);
 
     queryP2P(ctx0.device(), ctx1.device());
     queryP2P(ctx1.device(), ctx0.device());
 
-    void* buf0 = ctx0.initBuffer(elem_num);
-    void* buf1 = ctx1.initBuffer(elem_num);
+    void* buf0 = ctx0.initBuffer(data_count);
+    void* buf1 = ctx1.initBuffer(data_count);
     printf("buf0 = %p, buf1 = %p\n", buf0, buf1);
 
-    std::vector<uint32_t> hostBuf0(elem_num, 0);
+    std::vector<uint32_t> hostBuf0(data_count, 0);
     ctx0.copyBuffer(hostBuf0);
     printBuf(hostBuf0, 16);
 
-    std::vector<uint32_t> hostBuf1(elem_num, 0);
+    std::vector<uint32_t> hostBuf1(data_count, 0);
     ctx1.copyBuffer(hostBuf1);
     printBuf(hostBuf0, 16);
 
