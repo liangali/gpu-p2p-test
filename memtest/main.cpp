0feee4be31503f9f06c8f7b263671f320d8e685c
Li, Liang A <liang.a.li@intel.com>
2024-07-09 21:36:30 +0800

    verified >4GB memory read/write wroked well

--- memtest/main.cpp
+++ memtest/main.cpp
@@ -10,7 +10,14 @@ kernel void test_kernel(global int *buf0, global int *buf1) \
   const int id = get_global_id(0); \
   int offset = 1024*1024*1024; \
   buf1 += offset; \
-  buf1[id] = buf0[id] * 4; \
+  buf1[id] = buf0[id] * 2; \
+} \
+kernel void test_kernel2(global int *buf0, global int *buf1) \
+{ \
+  const int id = get_global_id(0); \
+  int offset = 1024*1024*1024; \
+  buf1 += offset; \
+  buf0[id] = buf1[id] * 3; \
 } \
 ";
 
@@ -27,12 +34,13 @@ int main(int argc, char** argv)
 
     cl_mem buf0 = oclctx.createBuffer(elemCount * sizeof(uint32_t), initBuf);
 
-    size_t sizeInBytes = 1.2 * 1024 * 1024 * 1024 * sizeof(uint32_t);
+    size_t sizeInBytes = 1.5 * 1024 * 1024 * 1024 * sizeof(uint32_t);
     cl_mem buf1 = oclctx.createBuffer(sizeInBytes);
     printf("buf size = %lld, buf handle = %p\n", sizeInBytes, buf1);
 
     oclctx.runKernel(test_kernel_code, "test_kernel", buf0, buf1, elemCount);
-    oclctx.printBuffer(buf1, 16, 4 * 1024 * 1024 * 1024);
+    oclctx.runKernel(test_kernel_code, "test_kernel2", buf0, buf1, elemCount);
+    oclctx.printBuffer(buf0, 16, 0);
 
     oclctx.freeBuffer(buf0);
     oclctx.freeBuffer(buf1);
